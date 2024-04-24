3203f22f345550449e18f7702fb0de22f6f65210
Albin <142149906+mkdew37@users.noreply.github.com>
2024-04-24 11:55:56 +0200

    Renamed some classes, added 3 clickevents, mousedown,mouseover and mouseup to draw

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 // Variable Declarations
-
-
+let drawingOn = false;
 // DOM elements
 const sketchPad = document.getElementById('pad');
 
-//Functions
 
+//Functions
 function createSketchPad () {
     for (let i = 0; i < 16; i++)    {
         const divRow = document.createElement('div');
-        divRow.classList.add('row');
+        divRow.classList.add('gridRow');
+     //   const rowId = 'row' + i;
+     //   divRow.setAttribute('id', rowId);
         sketchPad.appendChild(divRow);
 
+
         for (let j = 0; j<16; j++)  {
-            const divColumns = document.createElement('div');
-            divColumns.classList.add('column');
-            divRow.appendChild(divColumns);
+            const divColumn = document.createElement('div');
+            divColumn.classList.add('gridColumn');
+         //   const columnId = 'column' + (i * 16 + j);
+          //  divColumn.setAttribute('id', columnId);
+            divRow.appendChild(divColumn);
         }
     }
 }
 
+function activateDrawing () {
+    drawingOn = true;
+}
+
 createSketchPad();
+
+//Click Events
+sketchPad.addEventListener('mousedown', function(event) {
+    if (event.target.classList.contains('gridColumn')) {
+        activateDrawing();
+    }
+});
+
+sketchPad.addEventListener('mouseover', function(event) {
+    if (drawingOn && event.target.classList.contains('gridColumn'))  {
+        event.target.classList.add('onHover');
+    }
+});
+
+sketchPad.addEventListener('mouseup', function(event)   {
+    drawingOn = false;
+})
+})
