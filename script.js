13fad24b03550c27967fcb1453a9dbad1e8c575c
Albin <142149906+mkdew37@users.noreply.github.com>
2024-04-24 13:28:47 +0200

    Added a button that creates a new grid

--- script.js
+++ script.js
@@ -4,23 +4,34 @@ document.addEventListener('DOMContentLoaded', function() {
 let drawingOn = false;
 // DOM elements
 const sketchPad = document.getElementById('pad');
+const btnNewGrid = document.getElementById('newPad');
+const btnClear = document.getElementById('clear'); 
 
 
 //Functions
-function createSketchPad () {
+function createPad () {
     for (let i = 0; i < 16; i++)    {
         const divRow = document.createElement('div');
         divRow.classList.add('gridRow');
-     //   const rowId = 'row' + i;
-     //   divRow.setAttribute('id', rowId);
         sketchPad.appendChild(divRow);
-
 
         for (let j = 0; j<16; j++)  {
             const divColumn = document.createElement('div');
             divColumn.classList.add('gridColumn');
-         //   const columnId = 'column' + (i * 16 + j);
-          //  divColumn.setAttribute('id', columnId);
+            divRow.appendChild(divColumn);
+        }
+    }
+}
+
+function createNewPad(size)   {
+    for (let i = 0; i < size; i++)    {
+        const divRow = document.createElement('div');
+        divRow.classList.add('gridRow');
+        sketchPad.appendChild(divRow);
+
+        for (let j = 0; j<size; j++)  {
+            const divColumn = document.createElement('div');
+            divColumn.classList.add('gridColumn');
             divRow.appendChild(divColumn);
         }
     }
@@ -30,22 +41,53 @@ function activateDrawing () {
     drawingOn = true;
 }
 
-createSketchPad();
+
+createPad();
+
+
+
 
 //Click Events
-sketchPad.addEventListener('mousedown', function(event) {
+sketchPad.addEventListener('mousedown', event => {
     if (event.target.classList.contains('gridColumn')) {
         activateDrawing();
     }
 });
 
-sketchPad.addEventListener('mouseover', function(event) {
+sketchPad.addEventListener('mouseover', event => {
     if (drawingOn && event.target.classList.contains('gridColumn'))  {
         event.target.classList.add('onHover');
     }
 });
 
-sketchPad.addEventListener('mouseup', function(event)   {
+sketchPad.addEventListener('mouseup', () =>   {
     drawingOn = false;
-})
+});
+
+btnClear.addEventListener('click', () => {
+    const gridColumns = document.querySelectorAll('.gridColumn');
+    gridColumns.forEach(column => {
+        column.classList.remove('onHover');
+    });
+});
+
+btnNewGrid.addEventListener('click', () => {
+    let newGridSize = prompt('How many squares do you want your new pad to contain?');
+    if (newGridSize > 100) {
+        alert('You will crash the site if you choose that many, choose again!');
+        newGridSize = prompt('How many squares do you want your new pad to contain?');
+    } if (newGridSize) {
+        const gridColumns = document.querySelectorAll('.gridColumn');
+        gridColumns.forEach(column => {
+            column.classList.remove('onHover');
+        });
+        createNewPad(newGridSize);
+    }
+});
+
+
+
+
+
+
 })
